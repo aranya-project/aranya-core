8e14e76fe4e88fe2f72d26ae5a2d50298613d658
Jonathan Dygert <jdygert@spideroak.com>
2024-02-02 14:17:34 -0500

    ci: fix canaries (#548)
    For some reason nested std deps aren't causing duplicate panic handler
    anymore. It was hacky anyway. Compile for `aarch64-unknown-none` target
    instead. We can move canaries as well as clippy to just run on amd64.
    
    ---------
    
    Co-authored-by: Eric Lagergren <elagergren@spideroak.com>

--- canaries/canary-std/src/main.rs
+++ canaries/canary-std/src/main.rs
@@ -1,15 +1,6 @@
 #![no_std]
 #![no_main]
 
-extern crate base58;
-extern crate crypto;
-extern crate idam;
-extern crate policy_ast;
-// extern crate policy_lang;
-// extern crate policy_vm;
-extern crate runtime;
-extern crate service;
-
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
