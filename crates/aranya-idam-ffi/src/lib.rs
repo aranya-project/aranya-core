aac8b3af41ff6ca5dbc0d91694740c04a1562828
Scott McKeon <smckeon@spideroak.com>
2025-01-08 10:35:33 -0800

    Fix bare url lint

--- crates/aranya-idam-ffi/src/lib.rs
+++ crates/aranya-idam-ffi/src/lib.rs
@@ -14,10 +14,10 @@
 //! available by a particular module. For details on the
 //! cryptographic APIs used by IdAM, see [IDAM crypto].
 //!
-//! TODO: Once the idam_crypto doc gets open sourced this link should be updated. https://github.com/aranya-project/aranya-docs/issues/17
-//! [IDAM crypto]: https://git.spideroak-inc.com/spideroak-inc/aranya-docs/blob/idam-crypto-apis/src/idam_crypto.md
-//! [default policy]: https://github.com/aranya-project/aranya/blob/main/crates/aranya-daemon/src/policy.md
-//! [policy lang]: https://github.com/aranya-project/aranya-docs/blob/main/src/policy-v1.md
+//! TODO: Once the idam_crypto doc gets open sourced this link should be updated. <https://github.com/aranya-project/aranya-docs/issues/17>
+//! [IDAM crypto]: <https://git.spideroak-inc.com/spideroak-inc/aranya-docs/blob/idam-crypto-apis/src/idam_crypto.md>
+//! [default policy]: <https://github.com/aranya-project/aranya/blob/main/crates/aranya-daemon/src/policy.md>
+//! [policy lang]: <https://github.com/aranya-project/aranya-docs/blob/main/src/policy-v1.md>
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(not(any(test, doctest)), no_std)]
