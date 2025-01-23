bcfe27a1709d805d643e293b05390182d9ea17ea
Ben Zimmerman <bzimmerman@spideroak.com>
2025-01-23 10:54:53 -0500

    fix large sync test

--- crates/aranya-runtime/src/testing/dsl.rs
+++ crates/aranya-runtime/src/testing/dsl.rs
@@ -753,9 +753,9 @@ fn sync<SP: StorageProvider, A: DeserializeOwned + Serialize>(
 
     if let Some(cmds) = request_syncer.receive(&target[..len])? {
         received = request_state.add_commands(&mut request_trx, sink, &cmds)?;
+        request_state.commit(&mut request_trx, sink)?;
+        request_state.update_heads(storage_id, &cmds, response_cache)?;
     };
-
-    request_state.commit(&mut request_trx, sink)?;
 
     Ok((sent, received))
 }
