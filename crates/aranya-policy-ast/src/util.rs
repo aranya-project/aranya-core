7d8cd9e9d0973cd43894fbc34db95b06f7fd2b31
Chip Black <chip@spideroak.com>
2025-02-13 16:09:12 -0600

    fmt

--- crates/aranya-policy-ast/src/util.rs
+++ crates/aranya-policy-ast/src/util.rs
@@ -15,7 +15,10 @@ impl FactDefinition {
 impl FieldDefinition {
     /// Is this a hashable type (int, bool, string, or id)?
     pub fn is_hashable(&self) -> bool {
-        matches!(self.field_type, VType::Int | VType::Bool | VType::String | VType::Id)
+        matches!(
+            self.field_type,
+            VType::Int | VType::Bool | VType::String | VType::Id
+        )
     }
 }
 
@@ -23,11 +26,14 @@ impl Expression {
     /// Is this a literal expression?
     pub fn is_literal(&self) -> bool {
         match self {
-            Expression::Int(_) | Expression::String(_) | Expression::Bool(_) | Expression::EnumReference(_) => true,
+            Expression::Int(_)
+            | Expression::String(_)
+            | Expression::Bool(_)
+            | Expression::EnumReference(_) => true,
             Expression::Optional(o) => match o {
                 Some(e) => e.is_literal(),
                 None => true,
-            }
+            },
             Expression::NamedStruct(s) => s.fields.iter().all(|(_, e)| e.is_literal()),
             _ => false,
         }
