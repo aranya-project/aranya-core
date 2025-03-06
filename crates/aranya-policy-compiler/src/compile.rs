be66fe16125fcf790c1f4a0b89c6712f05050d35
nikki-aranya <npoelma@spideroak.com>
2025-03-06 11:01:40 -0700

    Stub Empty Open/Seal

--- crates/aranya-policy-compiler/src/compile.rs
+++ crates/aranya-policy-compiler/src/compile.rs
@@ -1641,10 +1641,7 @@ impl<'a> CompileState<'a> {
         locator: usize,
     ) -> Result<(), CompileError> {
         if command.seal.is_empty() {
-            return Err(self.err_loc(
-                CompileErrorType::Unknown(String::from("Empty/missing seal block in command")),
-                locator,
-            ));
+            return Ok(());
         }
 
         // fake a function def for the seal block
@@ -1691,10 +1688,7 @@ impl<'a> CompileState<'a> {
         locator: usize,
     ) -> Result<(), CompileError> {
         if command.open.is_empty() {
-            return Err(self.err_loc(
-                CompileErrorType::Unknown(String::from("Empty/missing open block in command")),
-                locator,
-            ));
+            return Ok(());
         }
 
         // fake a function def for the open block
