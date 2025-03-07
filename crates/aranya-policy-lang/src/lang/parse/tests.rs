34a03dd0f2b4e2ec35b26c228321251c70780799
Aleko Petkov <apetkov@spideroak.com>
2025-03-07 11:56:53 -0800

    Match arms now accept expressions (not just blocks).

--- crates/aranya-policy-lang/src/lang/parse/tests.rs
+++ crates/aranya-policy-lang/src/lang/parse/tests.rs
@@ -1789,9 +1789,7 @@ fn parse_match_expression() {
                     let x = true
                     : x
                 }
-                _ => {
-                    : false
-                }
+                _ => false
             }
         }
     "#;
@@ -1824,10 +1822,7 @@ fn parse_match_expression() {
                         AstNode::new(
                             ast::MatchArmExpression {
                                 pattern: MatchPattern::Default,
-                                expression: Expression::Block(
-                                    vec![],
-                                    Box::new(Expression::Bool(false))
-                                )
+                                expression: Expression::Bool(false)
                             },
                             173
                         )
