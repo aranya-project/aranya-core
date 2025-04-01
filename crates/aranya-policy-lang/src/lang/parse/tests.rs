0733c6147cf400fb7c605a7850a0f3ddb3e9875b
Aleko Petkov <apetkov@spideroak.com>
2025-04-01 13:18:51 -0700

    Allow field insertion for effects.

--- crates/aranya-policy-lang/src/lang/parse/tests.rs
+++ crates/aranya-policy-lang/src/lang/parse/tests.rs
@@ -370,6 +370,33 @@ fn parse_effect() -> Result<(), PestError<Rule>> {
 }
 
 #[test]
+fn test_parse_effect_with_field_insertion() {
+    let cases = [(
+        r#"struct Foo { x int }
+        effect Bar {
+            +Foo,
+            y int
+        }
+        "#,
+        vec![
+            ast::StructItem::StructRef("Foo".to_string()),
+            ast::StructItem::Field(ast::EffectFieldDefinition {
+                identifier: String::from("y"),
+                field_type: ast::VType::Int,
+                dynamic: false,
+            }),
+        ],
+    )];
+    for (case, expected) in cases {
+        let policy = parse_policy_str(case, Version::V2).expect("should parse");
+        assert_eq!(
+            policy.effects[0].inner.items, expected,
+            "case: {case:?} => {expected:?}"
+        );
+    }
+}
+
+#[test]
 #[allow(clippy::result_large_err)]
 fn parse_command() -> Result<(), PestError<Rule>> {
     let src = r#"
@@ -615,17 +642,17 @@ fn parse_policy_test() -> Result<(), ParseError> {
         vec![AstNode::new(
             ast::EffectDefinition {
                 identifier: String::from("Added"),
-                fields: vec![
-                    ast::EffectFieldDefinition {
+                items: vec![
+                    ast::StructItem::Field(ast::EffectFieldDefinition {
                         identifier: String::from("x"),
                         field_type: ast::VType::Int,
                         dynamic: true,
-                    },
-                    ast::EffectFieldDefinition {
+                    }),
+                    ast::StructItem::Field(ast::EffectFieldDefinition {
                         identifier: String::from("y"),
                         field_type: ast::VType::Int,
                         dynamic: false,
-                    },
+                    }),
                 ],
             },
             326,
