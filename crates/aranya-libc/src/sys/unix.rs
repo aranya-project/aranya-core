8ffa090f09bbbc8e7a944c428ea9c011e01095f2
nikki-aranya <npoelma@spideroak.com>
2025-04-08 16:01:56 -0600

    Make Clippy Happy

--- crates/aranya-libc/src/sys/unix.rs
+++ crates/aranya-libc/src/sys/unix.rs
@@ -103,8 +103,8 @@ pub fn fdopendir(fd: BorrowedFd<'_>) -> Result<RawDir, Errno> {
 
 /// See `readdir(2)`.
 pub fn readdir(dir: RawDir) -> Result<DirEntry, Errno> {
-    // `To distinguish between an end-of-directory condition or an error, you
-    // must set errno to zero before calling readdir.`
+    // SAFETY: `To distinguish between an end-of-directory condition or an
+    // error, you must set errno to zero before calling readdir.`
     unsafe { crate::errno::clear_errno(); }
     // SAFETY: FFI call, no invariants.
     let ret = unsafe { libc::readdir(dir) };
