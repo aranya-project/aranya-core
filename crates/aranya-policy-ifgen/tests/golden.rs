c1120cc61585ad1500cb66aa8f00b4ea2b93cd8c
Aleko Petkov <apetkov@spideroak.com>
2025-05-01 10:26:44 -0700

    Use compiled module in ifgen so we can support struct field insertion in macros.

--- crates/aranya-policy-ifgen/tests/golden.rs
+++ crates/aranya-policy-ifgen/tests/golden.rs
@@ -2,6 +2,7 @@
 
 use std::{io::Write, path::Path};
 
+use aranya_policy_compiler::Compiler;
 use aranya_policy_ifgen_build::generate_code;
 use aranya_policy_lang::lang::parse_policy_document;
 
@@ -13,7 +14,8 @@ fn dotest(name: &str) {
     let doc = std::fs::read_to_string(data.join(format!("{name}.md"))).unwrap();
     let doc = parse_policy_document(&doc).unwrap();
 
-    let rust_code = generate_code(&doc);
+    let module = Compiler::new(&doc).compile().unwrap();
+    let rust_code = generate_code(&module);
 
     let mut file = mint.new_goldenfile(format!("{name}.rs")).unwrap();
     write!(file, "{rust_code}").unwrap();
