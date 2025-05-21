32d69444c5a2454830a16452734594bd94f7b874
Eric Lagergren <elagergren@spideroak.com>
2025-05-21 14:22:17 -0700

    aranya-crypto: clean up dependencies
    We were importing both postcard and ciborium, which was overkill. Choose
    ciborium since CBOR is a standard and postcard is "just" some library.
    
    This also lets us get rid of `MaxSize`, which we didn't really use. That
    will help us get rid of some duplicate dependencies in later PRs.
    
    Signed-off-by: Eric Lagergren <elagergren@spideroak.com>

---
+++ crates/aranya-crypto/src/util.rs
@@ -0,0 +1,35 @@
+#![cfg(any(feature = "memstore", feature = "test_util"))]
+pub mod cbor {
+    extern crate alloc;
+
+    use alloc::vec::Vec;
+
+    pub use ciborium::*;
+    use serde::{de::DeserializeOwned, Serialize};
+
+    pub fn to_allocvec<T: Serialize>(data: &T) -> Result<Vec<u8>, ser::Error<()>> {
+        use ser::Error::*;
+        let mut out = Vec::new();
+        // NB: Remap errors because `ciborium_io` uses different
+        // generic parameters depending whether `std` is enabled.
+        // Yay.
+        into_writer(data, &mut out).map_err(|err| match err {
+            Io(_) => Io(()),
+            Value(v) => Value(v),
+        })?;
+        Ok(out)
+    }
+
+    pub fn from_bytes<T: DeserializeOwned>(data: &[u8]) -> Result<T, de::Error<()>> {
+        use de::Error::*;
+        // NB: Remap errors because `ciborium_io` uses different
+        // generic parameters depending whether `std` is enabled.
+        // Yay.
+        from_reader(data).map_err(|err| match err {
+            Io(_) => Io(()),
+            Syntax(v) => Syntax(v),
+            Semantic(a, b) => Semantic(a, b),
+            RecursionLimitExceeded => RecursionLimitExceeded,
+        })
+    }
+}
