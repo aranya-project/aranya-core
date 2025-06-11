ebbc289aab99130955d46253feadc9bf5e464be9
Jonathan Dygert <jdygert@spideroak.com>
2025-06-11 13:02:23 -0400

    reorder and add simple tests

--- crates/aranya-policy-ast/src/text/imp.rs
+++ crates/aranya-policy-ast/src/text/imp.rs
@@ -1,5 +1,114 @@
 use core::slice;
 
+// TODO(jdygert): Better repr to fit in 16 bytes.
+#[derive(Clone)]
+pub enum Repr {
+    Static(&'static str),
+    Inline { bytes: [u8; MAX_INLINE], len: u8 },
+    Heap(arc::ArcStr),
+}
+
+/// The max number of bytes that can fit in the inline variant without increasing `Repr`'s size.
+const MAX_INLINE: usize = 3 * size_of::<usize>() - 2;
+
+impl Repr {
+    pub const fn empty() -> Self {
+        Self::Static("")
+    }
+
+    pub const fn from_static(s: &'static str) -> Self {
+        Self::Static(s)
+    }
+
+    pub fn from_str(s: &str) -> Self {
+        let len = s.len();
+        if len <= MAX_INLINE {
+            let mut bytes = [0u8; MAX_INLINE];
+            bytes[..len].copy_from_slice(s.as_bytes());
+            Self::Inline {
+                bytes,
+                len: len as u8,
+            }
+        } else {
+            Self::Heap(arc::ArcStr::new(s))
+        }
+    }
+
+    pub const fn as_str(&self) -> &str {
+        match self {
+            Repr::Static(s) => s,
+            Repr::Inline { bytes, len } => {
+                debug_assert!((*len as usize) <= MAX_INLINE);
+                // SAFETY: We always ensure that `&bytes[..len]` is a valid string.
+                let s = unsafe { slice::from_raw_parts(bytes.as_ptr(), *len as usize) };
+                // SAFETY: We always ensure that `&bytes[..len]` is a valid string.
+                unsafe { core::str::from_utf8_unchecked(s) }
+            }
+            Repr::Heap(s) => s.as_ref(),
+        }
+    }
+}
+
+impl Default for Repr {
+    fn default() -> Self {
+        Self::Static("")
+    }
+}
+
+impl core::fmt::Debug for Repr {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.as_str().fmt(f)
+    }
+}
+
+impl PartialEq for Repr {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_str().eq(other.as_str())
+    }
+}
+
+impl Eq for Repr {}
+
+impl PartialOrd for Repr {
+    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Repr {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.as_str().cmp(other.as_str())
+    }
+}
+
+impl core::hash::Hash for Repr {
+    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+        self.as_str().hash(state)
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Repr {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        // No arc pooling for serde :(
+        // We use `Cow` so we can often avoid allocating when the string
+        // is directly present in the source bytes.
+        let s = <alloc::borrow::Cow<'de, str>>::deserialize(deserializer)?;
+        Ok(Self::from_str(&s))
+    }
+}
+
+impl serde::Serialize for Repr {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.as_str().serialize(serializer)
+    }
+}
+
 mod arc {
     //! Based on `std::sync::Arc`.
 
@@ -110,111 +219,26 @@ mod arc {
     }
 }
 
-// TODO(jdygert): Better repr to fit in 16 bytes.
-#[derive(Clone)]
-pub enum Repr {
-    Static(&'static str),
-    Inline { bytes: [u8; MAX_INLINE], len: u8 },
-    Heap(arc::ArcStr),
-}
+#[cfg(test)]
+mod test {
+    use proptest::prelude::*;
 
-/// The max number of bytes that can fit in the inline variant without increasing `Repr`'s size.
-const MAX_INLINE: usize = 3 * size_of::<usize>() - 2;
+    use super::*;
 
-impl Repr {
-    pub const fn empty() -> Self {
-        Self::Static("")
-    }
-
-    pub const fn from_static(s: &'static str) -> Self {
-        Self::Static(s)
-    }
-
-    pub fn from_str(s: &str) -> Self {
-        let len = s.len();
-        if len <= MAX_INLINE {
-            let mut bytes = [0u8; MAX_INLINE];
-            bytes[..len].copy_from_slice(s.as_bytes());
-            Self::Inline {
-                bytes,
-                len: len as u8,
-            }
-        } else {
-            Self::Heap(arc::ArcStr::new(s))
+    proptest! {
+        #[test]
+        fn proptest_repr(s in ".{1,25}") {
+            let repr = Repr::from_str(&s);
+            assert_eq!(repr.as_str(), s.as_str());
         }
     }
 
-    pub const fn as_str(&self) -> &str {
-        match self {
-            Repr::Static(s) => s,
-            Repr::Inline { bytes, len } => {
-                debug_assert!((*len as usize) <= MAX_INLINE);
-                // SAFETY: We always ensure that `&bytes[..len]` is a valid string.
-                let s = unsafe { slice::from_raw_parts(bytes.as_ptr(), *len as usize) };
-                // SAFETY: We always ensure that `&bytes[..len]` is a valid string.
-                unsafe { core::str::from_utf8_unchecked(s) }
-            }
-            Repr::Heap(s) => s.as_ref(),
-        }
-    }
-}
-
-impl Default for Repr {
-    fn default() -> Self {
-        Self::Static("")
-    }
-}
-
-impl core::fmt::Debug for Repr {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        self.as_str().fmt(f)
-    }
-}
-
-impl PartialEq for Repr {
-    fn eq(&self, other: &Self) -> bool {
-        self.as_str().eq(other.as_str())
-    }
-}
-
-impl Eq for Repr {}
-
-impl PartialOrd for Repr {
-    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Repr {
-    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
-        self.as_str().cmp(other.as_str())
-    }
-}
-
-impl core::hash::Hash for Repr {
-    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
-        self.as_str().hash(state)
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for Repr {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        // No arc pooling for serde :(
-        // We use `Cow` so we can often avoid allocating when the string
-        // is directly present in the source bytes.
-        let s = <alloc::borrow::Cow<'de, str>>::deserialize(deserializer)?;
-        Ok(Self::from_str(&s))
-    }
-}
-
-impl serde::Serialize for Repr {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        self.as_str().serialize(serializer)
+    #[test]
+    fn test_empty() {
+        let empty1 = Repr::empty();
+        let empty2 = Repr::from_str("");
+        assert_eq!(empty1.as_str(), "");
+        assert_eq!(empty2.as_str(), "");
+        assert_eq!(empty1, empty2);
     }
 }
