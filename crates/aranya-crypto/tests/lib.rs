331c25a6ff3814aa3e7f9ac37cdee01f3f95db2c
elagergren-spideroak <36516532+elagergren-spideroak@users.noreply.github.com>
2025-06-13 09:33:15 -0700

    crypto: fix references to `__unwrapped_inner` (#284)
    And actually add a test to make sure it's fixed.
    
    This is needed for https://github.com/aranya-project/aranya/issues/308
    
    Fixes #283
    
    Signed-off-by: Eric Lagergren <elagergren@spideroak.com>

--- crates/aranya-crypto/tests/lib.rs
+++ crates/aranya-crypto/tests/lib.rs
@@ -34,3 +34,29 @@ mod custom_id_tests {
         assert_eq!(id, got);
     }
 }
+
+mod unwrapped_tests {
+    use core::marker::PhantomData;
+
+    use aranya_crypto::{id::IdError, unwrapped, Id, Identified};
+
+    #[test]
+    fn test_unwrapped() {
+        struct Seed<CS> {
+            seed: [u8; 64],
+            _marker: PhantomData<CS>,
+        }
+        impl<CS> Identified for Seed<CS> {
+            type Id = Id;
+            fn id(&self) -> Result<Self::Id, IdError> {
+                Ok(Id::default())
+            }
+        }
+        unwrapped! {
+            name: Seed;
+            type: Seed;
+            into: |seed: Self| { seed.seed };
+            from: |seed: [u8; 64]| { Seed { seed, _marker: PhantomData } };
+        }
+    }
+}
