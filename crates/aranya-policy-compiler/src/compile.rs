26fdddb286ec1f8a722642302727d0cb534e49f5
Aleko Petkov <apetkov@spideroak.com>
2025-06-25 16:54:43 -0700

    The Cast instruction should be emitted for all `Indeterminate` types as well as `Struct`s.

--- crates/aranya-policy-compiler/src/compile.rs
+++ crates/aranya-policy-compiler/src/compile.rs
@@ -942,8 +942,6 @@ impl<'a> CompileState<'a> {
                                 rhs_ident.clone(),
                             )));
                         }
-
-                        self.append_instruction(Instruction::Cast(rhs_ident.clone()));
                     }
                     Typeish::Indeterminate => {}
                     Typeish::Type(_) => {
@@ -952,6 +950,8 @@ impl<'a> CompileState<'a> {
                         )));
                     }
                 }
+
+                self.append_instruction(Instruction::Cast(rhs_ident.clone()));
 
                 Typeish::Type(VType::Struct(rhs_ident.clone()))
             }
