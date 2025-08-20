db91c3278b44861e8d74478eecf4e55693c9102c
Jonathan Dygert <jdygert@spideroak.com>
2025-08-20 14:01:39 -0400

    cleanup

--- crates/aranya-runtime/src/vm_policy.rs
+++ crates/aranya-runtime/src/vm_policy.rs
@@ -121,7 +121,8 @@ use core::{borrow::Borrow, cell::RefCell, fmt};
 
 use aranya_policy_vm::{
     ActionContext, CommandContext, ExitReason, KVPair, Machine, MachineIO, MachineStack,
-    OpenContext, PolicyContext, RunState, Stack, Struct, Value, ast::Identifier,
+    OpenContext, PolicyContext, RunState, Stack, Struct, Value,
+    ast::{self, Identifier},
 };
 use buggy::{BugExt, bug};
 use spin::Mutex;
@@ -603,25 +604,13 @@ impl<E: aranya_crypto::Engine> Policy for VmPolicy<E> {
     ) -> Result<(), EngineError> {
         let VmAction { name, args } = action;
 
-        {
-            use aranya_policy_vm::ast::Persistence as P2;
+        let def = self
+            .machine
+            .action_defs
+            .get(&name)
+            .ok_or(EngineError::InternalError)?;
 
-            use crate::Persistence as P1;
-
-            let actual = self
-                .machine
-                .action_defs
-                .get(&name)
-                .ok_or(EngineError::InternalError)?
-                .persistence;
-            match (persistence, actual) {
-                (P1::Persistent, P2::Persistent) | (P1::Ephemeral, P2::Ephemeral) => {}
-                _ => {
-                    error!("expected {name} to be {persistence} but it was {actual}");
-                    return Err(EngineError::InternalError);
-                }
-            }
-        }
+        match_persistence(name.as_str(), persistence, def.persistence)?;
 
         let parent = match facts.head_address()? {
             Prior::None => None,
@@ -816,9 +805,9 @@ impl<T: fmt::Display> fmt::Debug for DebugViaDisplay<T> {
 fn match_persistence(
     name: &str,
     expected: crate::Persistence,
-    actual: aranya_policy_vm::ast::Persistence,
+    actual: ast::Persistence,
 ) -> Result<(), EngineError> {
-    use aranya_policy_vm::ast::Persistence as P2;
+    use ast::Persistence as P2;
 
     use crate::Persistence as P1;
 
