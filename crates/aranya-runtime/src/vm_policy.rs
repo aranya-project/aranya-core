f47dba93332528c5db0eda879ed9a9d3ce716f54
Aleko Petkov <apetkov@spideroak.com>
2025-09-05 16:00:16 -0700

    Add optional else clause to check statement. It should probably be required, but I don't want to break existing tests.

--- crates/aranya-runtime/src/vm_policy.rs
+++ crates/aranya-runtime/src/vm_policy.rs
@@ -308,8 +308,12 @@ impl<E: aranya_crypto::Engine> VmPolicy<E> {
             Ok(reason) => match reason {
                 ExitReason::Normal => Ok(()),
                 ExitReason::Yield => bug!("unexpected yield"),
-                ExitReason::Check => {
-                    info!("Check {}", self.source_location(&rs));
+                ExitReason::Check(msg) => {
+                    info!(
+                        "Check {}: {}",
+                        self.source_location(&rs),
+                        msg.unwrap_or_default()
+                    );
 
                     match placement {
                         CommandPlacement::OnGraphAtOrigin | CommandPlacement::OffGraph => {
@@ -357,8 +361,12 @@ impl<E: aranya_crypto::Engine> VmPolicy<E> {
         match rs.call_command_recall(this_data, envelope.into()) {
             Ok(ExitReason::Normal) => Err(EngineError::Check),
             Ok(ExitReason::Yield) => bug!("unexpected yield"),
-            Ok(ExitReason::Check) => {
-                info!("Recall failed: {}", self.source_location(rs));
+            Ok(ExitReason::Check(msg)) => {
+                info!(
+                    "Recall failed {}: {}",
+                    self.source_location(rs),
+                    msg.unwrap_or_default()
+                );
                 Err(EngineError::Check)
             }
             Ok(ExitReason::Panic) | Err(_) => {
@@ -398,8 +406,12 @@ impl<E: aranya_crypto::Engine> VmPolicy<E> {
                     })?)
                 }
                 ExitReason::Yield => bug!("unexpected yield"),
-                ExitReason::Check => {
-                    info!("Check {}", self.source_location(&rs));
+                ExitReason::Check(msg) => {
+                    info!(
+                        "Check {}: {}",
+                        self.source_location(&rs),
+                        msg.unwrap_or_default()
+                    );
                     Err(EngineError::Check)
                 }
                 ExitReason::Panic => {
@@ -666,7 +678,7 @@ impl<E: aranya_crypto::Engine> Policy for VmPolicy<E> {
                             EngineError::Panic
                         })? {
                             ExitReason::Normal => (),
-                            r @ (ExitReason::Yield | ExitReason::Check | ExitReason::Panic) => {
+                            r @ (ExitReason::Yield | ExitReason::Check(_) | ExitReason::Panic) => {
                                 error!("Could not seal command: {}", r);
                                 return Err(EngineError::Panic);
                             }
@@ -748,8 +760,12 @@ impl<E: aranya_crypto::Engine> Policy for VmPolicy<E> {
                             EngineError::InternalError
                         })?;
                     }
-                    ExitReason::Check => {
-                        info!("Check {}", self.source_location(&rs));
+                    ExitReason::Check(msg) => {
+                        info!(
+                            "Check {}: {}",
+                            self.source_location(&rs),
+                            msg.unwrap_or_default()
+                        );
                         return Err(EngineError::Check);
                     }
                     ExitReason::Panic => {
