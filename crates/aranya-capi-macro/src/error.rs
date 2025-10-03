d1db6bd7a5a9304ab855ccf7ebdfe13ed5e63afd
Nikki <npoelma@spideroak.com>
2025-10-03 12:58:42 -0600

    Update crates/aranya-capi-macro/src/error.rs
    Co-authored-by: Jonathan Dygert <jdygert@spideroak.com>

--- crates/aranya-capi-macro/src/error.rs
+++ crates/aranya-capi-macro/src/error.rs
@@ -23,7 +23,7 @@ pub(super) fn derive(item: TokenStream) -> Result<TokenStream> {
         let mut reprs: Vec<_> = item
             .attrs
             .iter()
-            .filter_map(|a| a.path().is_ident("repr").then_some(a.parse_args::<Repr>()))
+            .filter_map(|a| a.path().is_ident("repr").then(|| a.parse_args::<Repr>()))
             .collect::<Result<_>>()?;
         if reprs.len() > 1 {
             return Err(Error::new_spanned(
