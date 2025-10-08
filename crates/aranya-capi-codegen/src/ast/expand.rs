a388c664cf0537a300d22b9eb3c57ac9a4e952dd
gknopf <gknopf@spideroak.com>
2025-10-08 11:33:25 -0700

    rm ext variants for init and cleanup

--- crates/aranya-capi-codegen/src/ast/expand.rs
+++ crates/aranya-capi-codegen/src/ast/expand.rs
@@ -329,18 +329,7 @@ impl Ast {
 
         // Generate a constructor.
         if strukt.derives.contains(DeriveTrait::Init) {
-            self.add_init_constructor(
-                ctx,
-                &strukt.ident,
-                &old,
-                &Lifetimes::default(),
-                if strukt.ext_error.is_some() {
-                    Some(NoExtError::with_span(span))
-                } else {
-                    None
-                },
-                &cfg,
-            )?;
+            self.add_init_constructor(ctx, &strukt.ident, &old, &Lifetimes::default(), &cfg)?;
         }
 
         if let Some(Builds { ty }) = &mut strukt.builds {
@@ -362,18 +351,7 @@ impl Ast {
 
         // Generate a destructor.
         if strukt.derives.contains(DeriveTrait::Cleanup) {
-            self.add_destructor(
-                ctx,
-                &strukt.ident,
-                &old,
-                &Lifetimes::default(),
-                if strukt.ext_error.is_some() {
-                    Some(NoExtError::with_span(span))
-                } else {
-                    None
-                },
-                &cfg,
-            )?;
+            self.add_destructor(ctx, &strukt.ident, &old, &Lifetimes::default(), &cfg)?;
         }
 
         // TODO
@@ -923,14 +901,13 @@ impl Ast {
     }
 
     /// Generates an `init` constructor for `ty`.
-    #[instrument(skip_all, fields(%ty, ?no_ext_error))]
+    #[instrument(skip_all, fields(%ty))]
     fn add_init_constructor(
         &mut self,
         ctx: &Ctx,
         ty: &Ident,
         old: &Ident,
         lifetimes: &Lifetimes,
-        no_ext_error: Option<NoExtError>,
         cfg: &[&Attribute],
     ) -> Result<()> {
         trace!("generating `init` constructor");
@@ -940,6 +917,7 @@ impl Ast {
         let capi = &ctx.capi;
         let util = &ctx.util;
         let name = old.to_snake_case().with_suffix("_init");
+        let no_ext_error = Some(NoExtError::with_span(span));
 
         let doc = parse_doc! {r#"
 /// Initializes `{ty}`.
@@ -1023,14 +1001,13 @@ impl Ast {
     }
 
     /// Generates a destructor for `ty`.
-    #[instrument(skip_all, fields(%ty, ?no_ext_error))]
+    #[instrument(skip_all, fields(%ty))]
     fn add_destructor(
         &mut self,
         ctx: &Ctx,
         ty: &Ident,
         old: &Ident,
         lifetimes: &Lifetimes,
-        no_ext_error: Option<NoExtError>,
         cfg: &[&Attribute],
     ) -> Result<()> {
         trace!("generating destructor");
@@ -1044,6 +1021,7 @@ impl Ast {
             .to_snake_case()
             .with_suffix("_init")
             .with_prefix(&ctx.fn_prefix);
+        let no_ext_error = Some(NoExtError::with_span(span));
 
         let doc = parse_doc! {r#"
 /// Releases any resources associated with `ptr`.
