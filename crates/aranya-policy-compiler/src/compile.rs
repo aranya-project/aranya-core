def2a3b27b6f9c74685bdd4d6c2a8944dc587059
Aleko Petkov <apetkov@spideroak.com>
2025-11-07 09:32:21 -0800

    add FunctionReturn meta instruction to mark end-of-function.

--- crates/aranya-policy-compiler/src/compile.rs
+++ crates/aranya-policy-compiler/src/compile.rs
@@ -1949,6 +1949,8 @@ impl<'a> CompileState<'a> {
         if !self.instruction_range_contains(from..self.wp, |i| matches!(i, Instruction::Return)) {
             return Err(self.err_loc(CompileErrorType::NoReturn, function_node.span));
         }
+        // Mark the end of the function body before the exit
+        self.append_instruction(Instruction::Meta(Meta::FunctionEnd));
         // If execution does not hit a return statement, it will panic here.
         self.append_instruction(Instruction::Exit(ExitReason::Panic));
 
