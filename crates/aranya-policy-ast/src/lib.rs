8306bb29a54e5bec67d065beefc2066b704ce710
Aleko Petkov <apetkov@spideroak.com>
2025-11-26 15:02:14 -0800

    fix correctness

--- crates/aranya-policy-ast/src/lib.rs
+++ crates/aranya-policy-ast/src/lib.rs
@@ -6,7 +6,12 @@
 #![allow(unstable_name_collisions)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(not(any(test, doctest, feature = "std")), no_std)]
-#![warn(missing_docs)]
+
+// TODO rkyv seems to have trouble with struct-like enum variants, like TypeKind::Result; it issues
+// missing-docs warnings, even though the variant and its fields are documented. This warning can't
+// be suppressed at the enum (or file) level, so we turn off the warning... Until we find a better
+// solution.
+//#![warn(missing_docs)]
 
 extern crate alloc;
 
