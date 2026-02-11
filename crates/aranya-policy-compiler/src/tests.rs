aea6f9122e7f58c9c0fafea43c0a3511ca2e6f54
Aleko Petkov <apetkov@spideroak.com>
2026-02-10 16:18:14 -0800

    Validation can now output warnings in addition to errors. Unused vars are treated as warnings, i.e. they are reported, but don't cause validation to fail.

--- crates/aranya-policy-compiler/src/tests.rs
+++ crates/aranya-policy-compiler/src/tests.rs
@@ -2745,12 +2745,20 @@ fn test_validate_return() {
 
     for p in valid {
         let m = compile_pass(p);
-        assert!(!validate(&m));
+        assert!(
+            validate(&m).is_valid(true),
+            "Expected case to be valid: {}",
+            p
+        );
     }
 
     for p in invalid {
         let m = compile_pass(p);
-        assert!(validate(&m));
+        assert!(
+            !validate(&m).is_valid(true),
+            "Expected case to be invalid: {}",
+            p
+        );
     }
 }
 
@@ -2855,12 +2863,20 @@ fn test_validate_publish() {
 
     for p in valid {
         let m = compile_pass(&p);
-        assert!(!validate(&m), "Expected case to be valid: {}", p);
+        assert!(
+            validate(&m).is_valid(true),
+            "Expected case to be valid: {}",
+            p
+        );
     }
 
     for p in invalid {
         let m = compile_pass(&p);
-        assert!(validate(&m), "Expected case to be invalid: {}", p);
+        assert!(
+            !validate(&m).is_valid(true),
+            "Expected case to be invalid: {}",
+            p
+        );
     }
 }
 
@@ -3524,7 +3540,7 @@ fn test_unused_values() {
                 return 0
             }
             "#,
-            "unused variable(s): x",
+            "f: unused variable(s): `x`",
         ),
         (
             r#"
@@ -3536,7 +3552,7 @@ fn test_unused_values() {
                 return 0  // c is unused
             }
             "#,
-            "unused variable(s): c",
+            "g: unused variable(s): `c`",
         ),
         (
             r#"
@@ -3549,7 +3565,7 @@ fn test_unused_values() {
                 return a
             }
             "#,
-            "unused variable(s): x",
+            "bar: unused variable(s): `x`",
         ),
         (
             r#"
@@ -3561,7 +3577,7 @@ fn test_unused_values() {
                 return y
             }
             "#,
-            "unused variable(s): x, z",
+            "baz: unused variable(s): `x`, `z`",
         ),
         (
             r#"
@@ -3570,7 +3586,7 @@ fn test_unused_values() {
                 return 42
             }
             "#,
-            "unused variable(s): x",
+            "f: unused variable(s): `x`",
         ),
         (
             r#"
@@ -3579,7 +3595,7 @@ fn test_unused_values() {
                 return y
             }
             "#,
-            "unused variable(s): x, z",
+            "g: unused variable(s): `z`, `x`", // NOTE arg values are popped in reverse order
         ),
         (
             r#"
@@ -3601,7 +3617,22 @@ fn test_unused_values() {
                 return x
             }
             "#,
-            "unused variable(s): y",
+            "qux: unused variable(s): `y`",
+        ),
+        (
+            r#"
+            function f(n int) int {
+                let a = n
+                let b = saturating_add(n, 1)
+                if n > 0 {
+                    return a // b unused here
+                }
+                else {
+                    return b // a unused here
+                }
+            }
+            "#,
+            "f: unused variable(s): `a`, `b`",
         ),
     ];
 
@@ -3610,17 +3641,26 @@ fn test_unused_values() {
         let module = Compiler::new(&policy).compile().expect("should compile");
 
         if expected_msg.is_empty() {
-            // This case should NOT fail validation
+            let result = validate(&module);
             assert!(
-                !validate(&module),
-                "case #{i} should not have validation errors"
+                result.is_valid(true),
+                "case #{i} should have no validation errors, but got: {:?}",
+                result.num_errors
             );
         } else {
             // This case SHOULD fail validation
+            let result = validate(&module);
             assert!(
-                validate(&module),
-                "expected validation error for unused variable in case #{i}"
+                result.num_errors == 0 && result.num_warnings > 0,
+                "case #{i} should have passed, but got {} error(s) and {} warning(s)",
+                result.num_errors,
+                result.num_warnings,
             );
+            // assert_eq!(
+            //     &result.warnings[0].as_str(),
+            //     expected_msg,
+            //     "case #{i} should have expected unused variable warning"
+            // );
         }
     }
 }
--- crates/aranya-policy-compiler/src/validate.rs
+++ crates/aranya-policy-compiler/src/validate.rs
@@ -2,9 +2,22 @@ use aranya_policy_ast::Identifier;
 use aranya_policy_module::{LabelType, Module, ModuleData};
 
 use crate::{
-    ActionAnalyzer, FinishAnalyzer, FunctionAnalyzer, TraceAnalyzerBuilder, TraceFailure,
+    ActionAnalyzer, FailureLevel, FinishAnalyzer, FunctionAnalyzer, TraceAnalyzerBuilder,
     UnusedVarAnalyzer, ValueAnalyzer,
 };
+
+#[derive(Debug, Clone)]
+pub struct ValidationResult {
+    pub num_warnings: usize,
+    pub num_errors: usize,
+}
+
+impl ValidationResult {
+    /// Returns true if there are no errors, and (if `include_warnings` is true) no warnings.
+    pub fn is_valid(&self, include_warnings: bool) -> bool {
+        self.num_errors == 0 && (!include_warnings || self.num_warnings == 0)
+    }
+}
 
 /// Post-compilation validation. Ensure:
 /// - action branches publish a command
@@ -12,9 +25,12 @@ use crate::{
 /// - no unused variables
 /// - all function code paths return values
 /// - commands enter a finish block
-pub fn validate(module: &Module) -> bool {
+pub fn validate(module: &Module) -> ValidationResult {
     let ModuleData::V0(ref m) = module.data;
-    let mut failed = false;
+    let mut result = ValidationResult {
+        num_warnings: 0,
+        num_errors: 0,
+    };
 
     // Get all global variable names
     let global_names: Vec<Identifier> = m.globals.keys().cloned().collect();
@@ -38,40 +54,49 @@ pub fn validate(module: &Module) -> bool {
         let tracer = tracer.build();
 
         match tracer.trace(l) {
-            Ok(failures) => {
-                for TraceFailure {
-                    responsible_instruction,
-                    message,
-                    ..
-                } in failures
-                {
-                    print!("Trace `{}` policy: {}", l.name, message);
+            Ok(issues) => {
+                for issue in issues {
+                    let level = match issue.level {
+                        FailureLevel::Warning => "warning",
+                        FailureLevel::Error => "error",
+                    };
+                    print!("{} in `{} {}`", level, l.ltype, l.name);
                     if let Some(codemap) = &m.codemap {
-                        match codemap.span_from_instruction(responsible_instruction) {
+                        match codemap.span_from_instruction(issue.responsible_instruction) {
                             Ok(span) => {
                                 let (line, col) = span.start_linecol();
-                                println!(" at row {} col {}:", line, col);
+                                print!(" at row {} col {}", line, col);
+                                println!(": {}", issue.message);
                                 println!("{}", span.as_str());
                             }
                             Err(e) => {
                                 println!();
+                                print!(": {}", issue.message);
                                 println!(
                                     "  address {} is out of range in codemap: {}",
-                                    responsible_instruction, e
+                                    issue.responsible_instruction, e
                                 );
                             }
                         }
                     }
                     println!();
-                    failed = true;
+                    match issue.level {
+                        FailureLevel::Warning => {
+                            result.num_warnings = result.num_warnings.saturating_add(1);
+                        }
+                        FailureLevel::Error => {
+                            result.num_errors = result.num_errors.saturating_add(1);
+                        }
+                    }
                 }
             }
             Err(e) => {
                 println!("{e}");
-                return false;
+                result.num_errors = result.num_errors.saturating_add(1);
+                return result;
             }
         }
     }
 
-    failed
+    result
 }
