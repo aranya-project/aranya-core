016e6462a6aad72c73f205aaaa769330d83fb24c
Jonathan Dygert <jdygert@spideroak.com>
2026-03-25 12:29:36 -0400

    feedback

--- crates/aranya-tcp-syncer/src/lib.rs
+++ crates/aranya-tcp-syncer/src/lib.rs
@@ -1,4 +1,7 @@
-//! An implementation of the syncer using TCP.
+//! A minimal TCP syncer implementation.
+//!
+//! This is a simple and minimal example of syncer implementation. It does not represent best
+//! practice and omits encryption and authorization.
 
 #![warn(missing_docs)]
 
