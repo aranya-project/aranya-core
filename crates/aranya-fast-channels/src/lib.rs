c152b3f3908b7b328962c1e3019ddcd053a2e876
Jonathan Dygert <jdygert@spideroak.com>
2026-04-22 13:53:51 -0400

    Revert "afc: add `SealCtx` to fix `Seq` tracking (#480)"
    This reverts commit 0d5ec62ea43a58eabdc7726b9ea8ead4f535a6a3.

--- crates/aranya-fast-channels/src/lib.rs
+++ crates/aranya-fast-channels/src/lib.rs
@@ -24,13 +24,6 @@
 //! [`AranyaState`]. By default, AFC provides a state
 //! implementation backed by shared memory.
 //!
-//! # Notes
-//!
-//! AFC encrypts/seals each message with a deterministic nonce derived from a
-//! base nonce and sequence number. Sequence numbers should not be re-used in a given channel
-//! but it is possible to do so by passing a "new" [`AfcState::SealCtx`] to the seal methods
-//! on [`Client`].
-//!
 //! # Example
 //!
 //! The following example demonstrates two [`Client`]s encrypting
@@ -144,10 +137,7 @@
 //!     // Encryption has a little overhead, so make sure the
 //!     // ouput buffer is large enough.
 //!     let mut dst = vec![0u8; GOLDEN.len() + Client::<ReadState<CS>>::OVERHEAD];
-//!
-//!     // Create the ctx to pass in.
-//!     let mut ctx = afc_client_a.setup_seal_ctx(client_a_channel_id)?;
-//!     afc_client_a.seal(&mut ctx, &mut dst[..], GOLDEN.as_bytes())?;
+//!     afc_client_a.seal(client_a_channel_id, &mut dst[..], GOLDEN.as_bytes())?;
 //!     dst
 //! };
 //!
