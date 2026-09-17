d0993a9da7ffc396f6c7dceff66baa2e942e4e82
Chip <106176244+chip-so@users.noreply.github.com>
2026-09-17 21:17:11 +0000

    Module format V1 (#759)
    This updates the ModuleData::V1 format to match [the
    spec](https://github.com/aranya-project/aranya-docs/pull/202) and
    implements read/write to slice routines.
    
    Closes #756

--- crates/aranya-runtime/src/vm_policy.rs
+++ crates/aranya-runtime/src/vm_policy.rs
@@ -123,7 +123,7 @@ use aranya_crypto::BaseId;
 use aranya_policy_vm::{
     ActionContext, CommandContext, CommandDef, ConstValue, ExitReason, KVPair, Machine, MachineIO,
     MachineStack, OpenContext, Persistence, PolicyContext, RunState, Stack as _, Struct, Value,
-    ast::Identifier,
+    ast::Identifier, ffi_contract_validate,
 };
 use buggy::{BugExt as _, bug};
 use tracing::{error, info, instrument};
@@ -205,9 +205,8 @@ impl<CE> VmPolicy<CE> {
         engine: CE,
         ffis: Vec<Box<dyn FfiCallable<CE> + Send + 'static>>,
     ) -> Result<Self, VmPolicyError> {
-        if let Some(contract) = &machine.contract {
-            let schemas: Vec<_> = ffis.iter().map(|m| m.schema()).collect();
-            contract.validate(&schemas)?;
+        if let Some(module_ffis) = &machine.ffis {
+            ffi_contract_validate(module_ffis, ffis.iter().map(|m| m.schema()))?;
         } else {
             tracing::warn!("Module does not have contract; cannot validate FFI");
         }
