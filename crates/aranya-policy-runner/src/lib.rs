6422fcf0d2bef21b0cd8fe72a776d3c6c9720992
Jonathan Dygert <jdygert@spideroak.com>
2026-09-18 12:47:16 -0400

    MVP load policy module into store

--- crates/aranya-policy-runner/src/lib.rs
+++ crates/aranya-policy-runner/src/lib.rs
@@ -336,6 +336,7 @@ impl PolicyRunner {
                 let action = VmAction {
                     name: action_ident,
                     args: Cow::Borrowed(&schedule.preamble_values),
+                    policy: None,
                 };
                 sink.begin();
                 tracing::debug!("Calling run schedule item {i}");
